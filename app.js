53c7ba75ba269e549a1188a65eaf8dee5de81cab
fiati3ewty <fiati3ewty@gmail.com>
2023-04-28 13:58:20 +0700

    Check point

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.send('Hello EP4');
